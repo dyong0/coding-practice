fdeb36830ffaa71cf825d3b9c3b6590657a59a9a
Junyong Lee(dyong) <likedyong@gmail.com>
2018-05-14 09:51:13 +0900

    Solve codeforces#955 in psuedo code

---
+++ js/src/codeforces.955.sadpowers.ts
@@ -0,0 +1,8 @@
+// http://codeforces.com/problemset/problem/955/C
+
+// total queries = 0
+// for k in range(L, R + 1):
+//  find all prime factors
+//  partition the factors into all possible 2 groups, where one group > 0 and the other one > 1
+//  add (# of groups / 2) to the total queries
+// return the total queries
